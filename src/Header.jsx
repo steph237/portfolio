0276db22064ede9a4c22bf6b73e41158aeee456a
steph237 <stephaniewnkwatoh@gmail.com>
2022-06-01 16:26:42 +0100

    made header responsive

--- src/Header.jsx
+++ src/Header.jsx
@@ -27,20 +27,20 @@ export default function Navbar() {
         <>
           <div className="mx-auto px-2 sm:px-6 lg:px-8 shadow-lg">
             <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-red-800 hover:scale-110 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    <MenuIcon className="block h-6 w-6 " aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
               <div className="headerContent  ">
-                <div className="flex-shrink-0 flex items-center">
-                 <p className="text-2xl ml-6 ">MD</p>
+                <div className="flex-shrink-0 flex items-center sm:ml-6">
+                 <p className="text-2xl ml-6  ">MD</p>
                
                 </div>
                 <div className="hidden sm:block sm:ml-6 mr-0">
@@ -63,7 +63,7 @@ export default function Navbar() {
                   </div>      
                 </div>
                 <div className=" 
-                flex text-gray-600 space-x-9 mt-2 ">
+                flex text-gray-600 space-x-9 mt-2 hidden md:block">
                 
                 <FontAwesomeIcon icon = {faLinkedin} className = "hover:text-red-800 hover:scale-110"></FontAwesomeIcon>
                 <FontAwesomeIcon icon = {faInstagram} className = "hover:text-red-800 hover:scale-110"></FontAwesomeIcon>
@@ -84,8 +84,8 @@ export default function Navbar() {
                   href={item.href}
                   className={classNames(
                     item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      ? "text-red-800"
+                      : "text-gray-600  hover:text-red-800 ",
                     "block px-3 py-2 rounded-md text-base font-medium"
                   )}
                   aria-current={item.current ? "page" : undefined}
