ab17164481cf842c01879b645b6cd3d30c7c4366
steph237 <stephaniewnkwatoh@gmail.com>
2022-06-27 18:28:58 +0100

    added the about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,35 @@
 import React from "react";
-
+import Stephy from "../../stephy.jpeg";
 import "./about.css";
-
-
 
 const About = () => {
   return (
-    <div>
-      <h1> here is my about</h1>
+    <div className="about">
+      <div>
+        {" "}
+        <img src={Stephy} alt="background" />
+      </div>
+      <div className="about-info ">
+        <p className="text-xl inline">
+          {" "}
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, ex
+          voluptatem sint, molestias itaque placeat dicta vitae repellendus
+          ducimus quis quasi voluptate nemo nam excepturi harum facilis iure
+          incidunt! Esse! Lorem ipsum, dolor sit amet consectetur adipisicing
+          elit. Odit, ex voluptatem sint, molestias itaque placeat dicta vitae
+          repellendus ducimus quis quasi voluptate nemo nam excepturi harum
+          facilis iure incidunt! Esse! Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Odit, ex voluptatem sint, molestias itaque placeat
+          dicta vitae repellendus ducimus quis quasi voluptate nemo nam
+          excepturi harum facilis iure incidunt! Esse!
+        </p>
+        <div>
+          {" "}
+          <button class="mt-8 bg-red-800 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            View CV
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
