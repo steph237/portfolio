99e5025320e16da66f48d6a4a6f87778b38cd3fa
steph237 <stephaniewnkwatoh@gmail.com>
2022-07-28 15:02:13 +0100

    update:added the porfolio cards to portfolio

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,17 +2,24 @@
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Dropdown() {
+  const [selected, setSelected] = useState();
+
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-          Options
+        <Menu.Button
+          className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+          onChange={(e) => setSelected(e.target.value || null)}
+          value={selected || ""}
+        >
+          Sort
           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
       </div>
@@ -37,7 +44,7 @@ export default function Dropdown() {
                     "block px-4 py-2 text-sm"
                   )}
                 >
-                  Account settings
+                  UI/UX projects
                 </a>
               )}
             </Menu.Item>
@@ -50,38 +57,10 @@ export default function Dropdown() {
                     "block px-4 py-2 text-sm"
                   )}
                 >
-                  Support
+                  Applications
                 </a>
               )}
             </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                    "block px-4 py-2 text-sm"
-                  )}
-                >
-                  License
-                </a>
-              )}
-            </Menu.Item>
-            <form method="POST" action="#">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    type="submit"
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "block w-full text-left px-4 py-2 text-sm"
-                    )}
-                  >
-                    Sign out
-                  </button>
-                )}
-              </Menu.Item>
-            </form>
           </div>
         </Menu.Items>
       </Transition>
--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Dropdown from "../../components/Dropdown";
 import "../About/about.css";
-
+import { useState } from "react";
 import "./portfolio.css";
+import Portfoliocard from "../../components/Portfoliocard/Portfolio";
 
 const Portfolio = () => {
+  const [selected, setSelected] = useState();
+
   return (
     <div className="about">
-      <div className="about-content flex space-x-10">
+      <div className="about-content flex space-x-36">
         <div className="about-info ">
           <h1>Portfolio </h1>
           <h2>
@@ -23,10 +26,24 @@ const Portfolio = () => {
             have also contibuted to many open source projects
           </p>
         </div>
+        <div></div>
+
         <div>
-          <Dropdown />
+          <select
+            className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+            onChange={(e) => setSelected(e.target.value || null)}
+            value={selected || ""}
+          >
+            <option value="" selected disabled hidden>
+              Sort Projects
+            </option>
+
+            <option value="1">UI/UX design projects</option>
+            <option value="2">Applications </option>
+          </select>
         </div>
       </div>
+      <Portfoliocard />
       <hr size="20" align="center" />
     </div>
   );
