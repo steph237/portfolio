96a7ca954e0e2a57aa0f7b4cb8b4541d271d9898
steph237 <stephaniewnkwatoh@gmail.com>
2022-07-29 15:18:51 +0100

    added the contact form

---
+++ src/components/Form.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <form class="w-full max-w-lg">
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-first-name"
+            >
+              First Name
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+            />
+          </div>
+          <div class="w-full md:w-1/2 px-3">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-last-name"
+            >
+              Last Name
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+            />
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Email Address
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              type="text"
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Message
+            </label>
+            <textarea
+              class="appearance-none block w-full bg-gray-200 text-gray-700 borderrounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              type="text"
+              rows={5}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default Form;
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,47 @@
 import React from "react";
 
-import "./contact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faInstagram,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+
+import Form from "../../components/Form";
+// import { Facebook } from "@mui/icons-material";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="about">
+      <div className="about-content">
+        <div className="about-info ">
+          <h1>Contact</h1>
+          <h2> Send an Email or let's connect on social media </h2>
+          <br />
+          <div
+            className=" 
+                flex text-gray-600 space-x-9 mt-2 hidden md:block"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="hover:text-red-800 hover:scale-110"
+            ></FontAwesomeIcon>
+
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className="hover:text-red-800 hover:scale-110"
+            ></FontAwesomeIcon>
+
+            <FontAwesomeIcon
+              icon={faFacebook}
+              className="hover:text-red-800 hover:scale-110"
+            ></FontAwesomeIcon>
+          </div>
+        </div>
+
+        <Form />
+      </div>
+    </div>
+  );
 };
 export default Contact;
